d664de6f454a8c6173a2f57514dbcd16974c7752
cofcoltd <cofcoltd@qq.com>
2017-11-07 09:27:23 +0800

    信号栏

--- sd/wechat/js/singal.js
+++ sd/wechat/js/singal.js
@@ -2,13 +2,17 @@ $(function() {
 
 	setInterval(function() {
 		
+		//增长
 		var curr = $('#battery_body').width();
+		
 		if( curr >= 36) {
 			$('#battery_body').width(4);
 		} else {
 			$('#battery_body').width(curr + 2)
 		}
 		
+		
+		//变色
 		if($('#battery_body').width()<=10) {
 			$('#battery_body').css("background-color" , "red");
 		} else if( $('#battery_body').width()>10 && $('#battery_body').width()<28  ) {
