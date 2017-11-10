c3ad2ef1ac4d48a6cc175784133b76c52bd6ce09
cofcoltd <cofcoltd@qq.com>
2017-11-10 10:27:28 +0800

    底部菜单与 页面切换

---
+++ sd/wechat/js/router.js
@@ -0,0 +1,29 @@
+//当页面加载时
+$(function(){
+	
+//	/* 进入时加载 */	
+	$("#dynamic").load("dynamic/chat-list.html");
+
+	//点击聊天时
+	$("#menu-chat").click(function(){
+		
+		$("#dynamic").load("dynamic/chat-list.html");
+		$("#nav_center_content").html("微信(26)");
+		$("#function_img").show()
+	})
+	//点击发现时
+	$("#menu-discover").click(function(){
+		
+		$("#dynamic").load("dynamic/discover.html")
+		$("#nav_center_content").html("发现");
+		
+		$("#function_img").hide();
+		
+//		$("#function_img").hide(2000 , function(){
+//			alert("我没啦...")}
+//		)
+		
+	})
+	
+
+});
